f03fe30bc5e13ef26a95bf92eb4b24842a6b11e4
Segundo Fdez <segun.2@gmail.com>
2025-02-11 14:21:37 +0100

    improve ux navigation

--- src/js/image-navigation.js
+++ src/js/image-navigation.js
@@ -2,9 +2,27 @@ export function imageNavigation() {
     const projectInfo = document.querySelectorAll('.projects-item-info');
     const projectItem = document.querySelectorAll('.projects-item');
 
+    // Agregar la clase has-media al primer elemento por defecto
+    if (projectItem.length > 0) {
+        projectItem[0].classList.add('has-media');
+    }
+
     projectInfo.forEach((item, index) => {
-        item.addEventListener('click', () => {
+        item.addEventListener('click', (event) => {
             const media = projectItem[index];
+
+            // Verificar si el clic se realizó en una etiqueta <a>
+            if (event.target.tagName.toLowerCase() === 'a') {
+                // Si el elemento no tiene la clase has-media, permitir el despliegue
+                if (!media.classList.contains('has-media')) {
+                    projectItem.forEach(media => {
+                        media.classList.remove('has-media');
+                    });
+                    media.classList.add('has-media');
+                    item.scrollIntoView({ block: 'start' });
+                }
+                return;
+            }
 
             if (media.classList.contains('has-media')) {
                 // Si el elemento ya tiene la clase has-media, la quitamos
