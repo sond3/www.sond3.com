0c1f9fb3f403da53abbb9b859f0dab6f6fb7736a
Segundo Fdez <segun.2@gmail.com>
2025-02-19 22:01:10 +0100

    add more imgs and lazy js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { setYear } from "./js/set-year.js";
 // import { setRandomBackground, getContrastingColor } from './js/g et-colors.js';
 import { copyImageToInfo } from "./js/image-follow.js";
 import { imageNavigation } from "./js/image-navigation.js";
+import { animateImagesOnLoad } from "./js/animate-lazy-load-img.js";
 
 document.addEventListener("DOMContentLoaded", function () {
     // setRandomBackground();
     // getContrastingColor();
+    animateImagesOnLoad();
     imageNavigation();
     copyImageToInfo();
     setYear();
